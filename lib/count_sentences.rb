a57a027dafa8f268c364952eb2ccde5337f8a8d9
clifford <clifford@noukod.com>
2018-10-02 19:39:56 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if end_with?(".")
+      return true
+    else
+      return false
   end
+end
 
   def question?
-
+    if end_with?("?")
+      return true
+    else
+      return false
   end
+end
 
   def exclamation?
-
+      if end_with?("!")
+        return true
+      else
+        return false
   end
 
   def count_sentences
-
+    a_array = self.split(/[.?!]/)
+   a_array.delete_if{|sentence| sentence.empty?}
+   a_array.length
   end
 end
